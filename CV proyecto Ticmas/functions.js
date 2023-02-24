50d685b9b6f2ba168e6315cb12968bd957b4b00b
fakumetal <114892011+fakumetal@users.noreply.github.com>
2023-02-24 19:23:05 -0300

    Add files via upload

---
+++ CV proyecto Ticmas/functions.js
@@ -0,0 +1,13 @@
+function redirigirFB() {
+  window.open("https://www.facebook.com", "_blank");
+}
+function redirigirTW() {
+  window.open("https://www.twitter.com", "_blank");
+}
+function redirigirLD() {
+  window.open("https://ar.linkedin.com", "_blank");
+}
+
+function redirigirIT() {
+  window.open("https://www.instagram.com", "_blank");
+}
